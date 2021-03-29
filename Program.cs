8ede8cc7a7e65318b4d3a48c19acd67ac998c569
zepanzeri <zepanzeri94@gmail.com>
2021-03-29 16:49:32 -0300

    Implemented all the bank related methods.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dio_bank
 {
   class Program
   {
+    static List<Account> accountList = new List<Account>();
     static void Main(string[] args)
     {
       int option;
@@ -37,6 +39,64 @@ namespace dio_bank
             Console.ReadLine();
         }
       } while (option != 0);
+    }
+
+    private static void Transfer()
+    {
+      System.Console.WriteLine("Transfer");
+      System.Console.WriteLine("Inform the origin account index:");
+      int originIndex = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("Inform the target account index:");
+      int targetIndex = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("Type the ammount you want to transfer:");
+      double ammount = double.Parse(Console.ReadLine());
+      accountList[originIndex].Transfer(ammount, accountList[targetIndex]);
+    }
+
+    private static void Deposit(){
+      System.Console.WriteLine("Deposit");
+      System.Console.WriteLine("Inform the account index:");
+      int accountIndex = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("Type the ammount you wish to deposit:");
+      double ammount = double.Parse(Console.ReadLine());
+      accountList[accountIndex].Deposit(ammount);
+    }
+
+    private static void Withdraw()
+    {
+      System.Console.WriteLine("Withdraw");
+      System.Console.WriteLine("Inform the account index:");
+      int accountIndex = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("Type the ammount you wish to withdraw:");
+      double ammount = double.Parse(Console.ReadLine());
+      accountList[accountIndex].Withdraw(ammount);
+    }
+
+    private static void ListAccount()
+    {
+      System.Console.WriteLine("Account List");
+      if(accountList.Count==0){
+        System.Console.WriteLine("No accounts registered");
+        return;
+      }
+      foreach(Account acc in accountList){
+        System.Console.WriteLine(acc);
+      }
+    }
+
+    private static void CreateAccount()
+    {
+      System.Console.WriteLine("Create Account");
+      System.Console.WriteLine("Type 1 for Personal Account or 2 for Business Account:");
+      int accountType = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("Type the client name:");
+      string name = Console.ReadLine();
+      System.Console.WriteLine("Type the initial balance:");
+      double balance = double.Parse(Console.ReadLine());
+      System.Console.WriteLine("Type the credit:");
+      double credit = double.Parse(Console.ReadLine());
+      Account newAccount = new Account(name, balance, credit, (AccountType)accountType);
+      accountList.Add(newAccount);
     }
 
     private static int UserOption()
